0e2d2d775e3fccb89b5cef8700ad875885340e84
Tiago de A Fernandes <tfernandes@mprj.mp.br>
2019-07-29 13:31:09 -0300

    added ngrx and planet component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
+import { PlanetsComponent } from './planets/planets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsComponent
   ],
   imports: [
     CoreModule,
