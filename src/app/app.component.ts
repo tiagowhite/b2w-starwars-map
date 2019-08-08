9e4e3ff9389c3d6ab5898a261642bdd9e08383c4
Tiago de A Fernandes <tfernandes@mprj.mp.br>
2019-08-08 15:20:16 -0300

    closes #1 - refresh and added responsive sidenav behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'b2w-starwars-map';
+export class AppComponent implements OnDestroy {
+  opened = true;
+  over = 'side';
+  /*expandHeight = '42px';
+  collapseHeight = '42px';
+  displayMode = 'flat';
+  overlap = false;*/
+  activeMediaQuery = '';
+  watcher: Subscription;
+
+  constructor(mediaObserver: MediaObserver) {
+    this.watcher = mediaObserver.media$.subscribe((change: MediaChange) => {
+      this.activeMediaQuery = change ? `'${change.mqAlias}' = (${change.mediaQuery})` : '';
+      if (change.mqAlias === 'sm' || change.mqAlias === 'xs') {
+        this.opened = false;
+        this.over = 'over';
+      } else {
+        this.opened = true;
+        this.over = 'side';
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.watcher.unsubscribe();
+  }
+
+
 }
