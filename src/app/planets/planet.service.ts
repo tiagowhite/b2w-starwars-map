0780fd2c9764dad3f104d903e7f357f22c7c2df9
Tiago de A Fernandes <tfernandes@mprj.mp.br>
2019-08-13 15:29:46 -0300

    some rxjs tests

--- src/app/planets/planet.service.ts
+++ src/app/planets/planet.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import {environment} from '../../environments/environment';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanetService {
   planetUrl = `${environment.swapi}planets`;
+  testUrl = 'https://swapi.co/api/planets/?format=json&page=';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   public getPlanets<T>(): Observable<T> {
     return this.http.get<T>(this.planetUrl);
   }
+
+  public test<T>(): Observable<T> {
+    return this.http.get<T>(this.testUrl);
+  }
+
 }
--- src/app/planets/planets.component.ts
+++ src/app/planets/planets.component.ts
@@ -3,6 +3,9 @@ import { selectPlanetList } from '../core/store/selectors/planet.selector';
 import { AppState } from '../core/store/state/app.state';
 import { select, Store } from '@ngrx/store';
 import { GetPlanets } from '../core/store/actions/planet.actions';
+import { PlanetService } from './planet.service';
+import { Planets } from './planets';
+import { log } from 'util';
 
 @Component({
   selector: 'app-planets',
@@ -12,13 +15,23 @@ import { GetPlanets } from '../core/store/actions/planet.actions';
 export class PlanetsComponent implements OnInit {
 
   planetList = this.store.pipe(select(selectPlanetList));
+  test: { orbital_period: number; surface_water: number; diameter: number; gravity: string; name: string; residents: []; climate: string; id: number; rotation_period: number; terrain: string; url: "https://swapi.co/api/planets/"; population: number }[];
 
-  constructor(private store: Store<AppState>) {
+  constructor(private store: Store<AppState>, private planetService: PlanetService) {
   }
 
   ngOnInit() {
-    this.store.dispatch(new GetPlanets());
+    /*this.store.dispatch(new GetPlanets());*/
+    this.runTest();
   }
 
 
+  private runTest() {
+    this.planetService.test<Planets>().subscribe(
+      (test: Planets) => {
+        this.test = test.results.map((obj, index) => ({...obj, id: index }));
+        log(this.test);
+      }
+    );
+  }
 }
