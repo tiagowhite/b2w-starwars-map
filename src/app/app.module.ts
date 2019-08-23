8c878cf50935870bbf33187e6ed34a3c962e7ed5
Tiago de A Fernandes <tfernandes@mprj.mp.br>
2019-08-23 17:32:14 -0300

    layout things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,43 +7,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { PlanetService } from './planets/planet.service';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { MaterialModule } from './core/material.module';
 import { StoreModule } from '@ngrx/store';
-import { appReducers } from './core/store/reducers/app.reducers';
 import { EffectsModule } from '@ngrx/effects';
 import { PlanetEffect } from './core/store/effects/planet.effect';
 import { ConfigEffect } from './core/store/effects/config.effect';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { appReducers } from './core/store/reducers/app.reducers';
 import { environment } from '../environments/environment';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent
+    AppComponent
   ],
   imports: [
     CoreModule,
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
     StoreModule.forRoot(appReducers),
     EffectsModule.forRoot([PlanetEffect, ConfigEffect]),
     StoreRouterConnectingModule.forRoot({stateKey: 'router'}),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [PlanetService],
   bootstrap: [AppComponent]
--- src/app/planets/planets.component.ts
+++ src/app/planets/planets.component.ts
@@ -2,12 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { selectPlanetList } from '../core/store/selectors/planet.selector';
 import { AppState } from '../core/store/state/app.state';
 import { select, Store } from '@ngrx/store';
-import { PlanetService } from './planet.service';
-import { Planets } from './planets';
-import { Planet } from './planet';
-import { of, pipe } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
-import { debug, log } from 'util';
+import { GetPlanets } from '../core/store/actions/planet.actions';
 
 
 @Component({
@@ -16,24 +11,13 @@ import { debug, log } from 'util';
   styleUrls: ['./planets.component.scss']
 })
 export class PlanetsComponent implements OnInit {
-
   planetList = this.store.pipe(select(selectPlanetList));
-  protected tempList = [];
 
-  constructor(private store: Store<AppState>, private planetService: PlanetService) {
+  constructor(private store: Store<AppState>) {
   }
 
   ngOnInit() {
-    /*this.store.dispatch(new GetPlanets());*/
+    this.store.dispatch(new GetPlanets());
   }
-
-
-  /*private runTest() {
-    this.planetService.test<Array<any>>().subscribe(
-      (data: []) => {
-        log(data);
-      }
-    );
-  }*/
 
 }
