aec9c45bac62647a6d3912e2a8703a38b7b4c6cc
Tiago de A Fernandes <tfernandes@mprj.mp.br>
2019-09-16 16:02:29 -0300

    Reading external json

--- scraper/orbital-planet-beacom.py
+++ scraper/orbital-planet-beacom.py
@@ -1,4 +1,5 @@
 # /usr/bin/python3
+
 from datetime import datetime
 from bs4 import BeautifulSoup
 from termcolor import colored
@@ -8,21 +9,17 @@ import json
 
 now = datetime.now()
 date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+
+# banner
 ascii_banner = pyfiglet.figlet_format("Planet Beacom")
-
-# json structure
-assets = {'planet': []}
-
 print(ascii_banner)
 
-planets = ["Alderaan", "Yavin_IV", "Hoth", "Dagobah", "Bespin", "Endor", "Naboo", "Coruscant", "Kamino",
-           "Geonosis", "Utapau", "Mustafar", "Kashyyyk", "Polis_Massa", "Mygeeto", "Felucia",
-           "Cato_Neimoidia", "Saleucami", "Stewjon", "Eriadu", "Corellia", "Rodia", "Nal_Hutta", "Dantooine",
-           "Bestine_IV", "Ord_Mantell", "Trandosha", "Socorro", "Mon_Cala", "Chandrila", "Sullust",
-           "Toydaria", "Malastare", "Dathomir", "Ryloth", "Aleen_Minor", "Vulpter", "Troiken", "Tund",
-           "Haruun_Kal", "Cerea", "Glee_Anselm", "Iridonia", "Tholoth", "Iktotch", "Champala", "Mirial",
-           "Serenno", "Concord Dawn", "Zolan", "Ojom", "Skako", "Muunilinst", "Shili", "Kalee", "Tatooine",
-           "Jakku"]
+# assets json
+assets = {'planets': []}
+
+# Open planets json
+with open('planets.json') as json_file:
+    planets = json.load(json_file)
 
 # get the planet data
 for planet in planets:
@@ -50,11 +47,10 @@ for planet in planets:
             with open(file_name, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=1024 * 1024):
                     if chunk:
-                        for i in range(100):
-                            f.write(chunk)
-                        assets['planet'].append({'name': planet, 'image': file_name})
-                        with open('assets.json', 'w') as outfile:
-                            json.dump(assets, outfile)
+                        f.write(chunk)
+                    assets['planets'].append({'name': planet, 'path': f.name})
+                    with open('assets.json', 'w') as outfile:
+                        json.dump(assets, outfile)
                         print(colored("[" + "DONE" + "]", 'blue'))
         except AttributeError:
             print(colored('ERROR!!! Image not found..: ' + planet, 'red'))
