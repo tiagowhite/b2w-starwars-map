83bfc4eee1e6fd6b68c2cb522266a740e5feddfe
Tiago de A Fernandes <tfernandes@mprj.mp.br>
2019-09-17 13:47:35 -0300

    scraper clean

--- scraper/orbital-planet-beacom.py
+++ scraper/orbital-planet-beacom.py
@@ -10,48 +10,59 @@ import json
 now = datetime.now()
 date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
 
+# target url to scrape
+url = 'https://starwars.fandom.com/wiki/{0}'
+
 # banner
 ascii_banner = pyfiglet.figlet_format("Planet Beacom")
-print(ascii_banner)
+# print(ascii_banner)
 
-# assets json
+# define assets json
 assets = {'planets': []}
 
+
 # Open planets json
-with open('planets.json') as json_file:
-    planets = json.load(json_file)
+def read_json_list():
+    with open('planets.json') as json_file:
+        return json.load(json_file)
 
-# get the planet data
-for planet in planets:
-    url = 'https://starwars.fandom.com/wiki/' + planet
-    req = requests.get(url)
 
-    # load data into bs4
-    soup = BeautifulSoup(req.text, 'html.parser')
+# define planets array
+planets = read_json_list()
 
-    try:
-        planet_image = soup.find('figure', {'class': 'pi-image'})
-        data_image_attrs = planet_image.find('img').attrs
-        image_name = data_image_attrs['data-image-name']
-    except AttributeError:
-        print(colored('ERROR IN PLANET: ' + planet, 'red'))
-        pass
-    else:
+
+def scraper(planets_array, target_url):
+    for planet in planets_array:
+        # loop through planets array and change url accordingly
+        req = requests.get(target_url.format(planet))
+        # load data into BeautifulSoup
+        soup = BeautifulSoup(req.text, 'html.parser')
+
         try:
-            image_link = planet_image.find('a')['href']
-            file_name = image_name.split('/')[-1].replace(" ", "_")
-            r = requests.get(image_link, stream=True)
-            log = "Saving... [" + date_time + "]" + "[" + file_name + "]"
-            print(colored(log, 'green'))
-
-            with open(file_name, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=1024 * 1024):
-                    if chunk:
-                        f.write(chunk)
-                    assets['planets'].append({'name': planet, 'path': f.name})
-                    with open('assets.json', 'w') as outfile:
-                        json.dump(assets, outfile)
-                        print(colored("[" + "DONE" + "]", 'blue'))
+            planet_image = soup.find('figure', {'class': 'pi-image'})
+            data_image_attrs = planet_image.find('img').attrs
+            image_name = data_image_attrs['data-image-name']
         except AttributeError:
-            print(colored('ERROR!!! Image not found..: ' + planet, 'red'))
+            print(colored('ERROR IN PLANET: ' + planet, 'red'))
             pass
+        else:
+            try:
+                image_link = planet_image.find('a')['href']
+                file_name = image_name.split('/')[-1].replace(" ", "_")
+                r = requests.get(image_link, stream=True)
+                log = "Saving... [" + date_time + "]" + "[" + file_name + "]"
+                print(colored(log, 'green'))
+                with open(file_name, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=1024 * 1024):
+                        if chunk:
+                            f.write(chunk)
+                        assets['planets'].append({'name': planet, 'path': f.name})
+                        with open('assets.json', 'w') as outfile:
+                            json.dump(assets, outfile)
+                            print(colored("[" + "DONE" + "]", 'blue'))
+            except AttributeError:
+                print(colored('ERROR!!! Image not found..: ' + planet, 'red'))
+                pass
+
+
+scraper(planets, url)
